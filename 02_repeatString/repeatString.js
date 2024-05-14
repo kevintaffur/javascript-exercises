76f4e13253f5ec0439f994f420412b17c78d408a
Kevin <86487929+kevintaffur@users.noreply.github.com>
2024-05-14 17:57:02 -0500

    exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+  if (times < 0) {
+    return "ERROR";
+  }
+  let txt = "";
+  for (let i = 0; i < times; i++) {
+    txt += str;
+  }
+  return txt;
 };
 
 // Do not edit below this line
