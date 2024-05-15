98db91f1d6d02f8cf6cdda835aad5e16942624fa
Kevin <86487929+kevintaffur@users.noreply.github.com>
2024-05-14 19:31:40 -0500

    exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+  if (typeof n1 !== "number" || typeof n2 !== "number") {
+    return "ERROR";
+  }
+  if (n1 < 0 || n2 < 0) {
+    return "ERROR";
+  }
 
+  let minNumber = n1;
+  let maxNumber = n2;
+  if (n2 < n1) {
+    minNumber = n2;
+    maxNumber = n1;
+  }
+  let sum = 0;
+  for (let i = minNumber; i <= maxNumber; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
