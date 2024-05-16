97c1162d63019d95cc1fff4be28479f9bb15f6b6
Kevin <86487929+kevintaffur@users.noreply.github.com>
2024-05-16 11:14:26 -0500

    exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  const arr = [];
+  let txt = text.toLowerCase();
+  txt = [...txt].filter((char) => {
+    return char !== " " && char !== "," && char !== "." && char !== "?" && char !== "!";
+  })
 
+  let reverseIndex = txt.length - 1;
+  for (let i = 0; i < txt.length; i++) {
+    arr[i] = txt[reverseIndex];
+    reverseIndex--;
+  }
+
+  const charsToEvaluate = Math.ceil(txt.length / 2);
+
+  for (let i = 0; i < charsToEvaluate; i++) {
+    if (txt[i] !== arr[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
