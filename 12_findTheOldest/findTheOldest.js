7a11ed0d7a06326267100cc2c8045d252ba95aa7
Kevin <86487929+kevintaffur@users.noreply.github.com>
2024-05-16 12:14:28 -0500

    exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let oldest = people[0];
+  people.map((person) => {
+    const age = getAge(person);
+    const oldestAge = getAge(oldest);
+    if (age > oldestAge) {
+      oldest = person;
+    }
+  });
+  return oldest;
 };
+
+const getAge = function(person) {
+  if (person.yearOfDeath === undefined) {
+    const currentYear = new Date().getFullYear();
+    return currentYear - person.yearOfBirth;
+  }
+  return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
