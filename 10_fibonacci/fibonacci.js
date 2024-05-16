153ad852f40e049bafccde82e428b490e6e3efaa
Kevin <86487929+kevintaffur@users.noreply.github.com>
2024-05-16 11:36:33 -0500

    exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (typeof n === "string") {
+    n = parseInt(n);
+  }
+  if (n === 0) {
+    return 0;
+  }
+  if (n < 0) {
+    return "OOPS";
+  }
+  if (n === 1) {
+    return 1;
+  }
+  const fib = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i-1] + fib[i-2];
+  }
+  return fib[fib.length-1]
 };
 
 // Do not edit below this line
